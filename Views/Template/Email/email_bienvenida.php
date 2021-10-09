7eba254cedf1a14a4d8fec8e50b0ac13a6b5708b
zayra-del <60948379+zayra-del@users.noreply.github.com>
2021-10-09 00:06:15 -0600

    Add files via upload

---
+++ Views/Template/Email/email_bienvenida.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Bienvenido Usuario</title>
+	<style type="text/css">
+		p{
+			font-family: arial;
+			letter-spacing: 1px;
+			color: #7f7f7f;
+			font-size: 15px;
+		}
+		a{
+			color: #d8827e;
+			font-family: arial;
+			text-decoration: none;
+			text-align: center;
+			display: block;
+			font-size: 18px;
+		}
+		.x_sgwrap p{
+			font-size: 20px;
+		    line-height: 32px;
+		    color: #e66465;
+		    font-family: arial;
+		    text-align: center;
+		}
+		.x_title_gray {
+		    color: #e5786b;
+		    padding: 5px 0;
+		    font-size: 15px;
+			border-top: 1px solid #CCC;
+		}
+		.x_title_blue {
+		    padding: 08px 0;
+		    line-height: 25px;
+		    text-transform: uppercase;
+			border-bottom: 1px solid #CCC;
+		}
+		.x_title_blue h1{
+			color: #e5786b;
+			font-size: 25px;
+			font-family: 'arial';
+		}
+		.x_bluetext {
+		    color:  #e66465; !important;
+		}
+		.x_title_gray a{
+			text-align: center;
+			padding: 10px;
+			margin: auto;
+			color: #e5786b;
+		}
+		.x_text_white a{
+			color: #FFF;
+		}
+		.x_button_link {
+		    width: 100%;
+			max-width: 470px;
+		    height: 40px;
+		    display: block;
+		    color: #FFF;
+		    margin: 20px auto;
+		    line-height: 40px;
+		    text-transform: uppercase;
+		    font-family: Arial Black,Arial Bold,Gadget,sans-serif;
+		}
+		.x_link_blue {
+		    background-color: #d8827e;
+		}
+		.x_textwhite {
+		    background-color: rgb(240, 128, 128);
+		    color: #ffffff;
+		    padding: 10px;
+		    font-size: 15px;
+		    line-height: 20px;
+		}
+	</style>
+</head>
+<body>
+	<table align="center" cellpadding="0" cellspacing="0" bgcolor="#ffffff" style="text-align:center;">
+		<tbody>
+			<tr>
+				<td>
+					<div class="x_sgwrap x_title_blue">
+						<h1><?= NOMBRE_EMPESA ?></h1>
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<div class="x_sgwrap">
+						<p>Hola <?= $data['nombreUsuario']; ?></p>
+					</div>
+					<p>Bienvenido a Sakura Shop</p>
+					<p>Ahora puedes ralizar tus compras en una página web.</p>
+					<p>Accede a tu cuenta con tus credenciales:</p>
+					<p>Usuario: <strong><?= $data['email']; ?></strong></p>
+					<p>Contraseña: <strong><?= $data['password']; ?></strong></p>
+					<a href="<?= BASE_URL; ?>" target="_blank" class="x_button_link x_link_blue">Comprar ahora</a>
+					<p class="x_title_gray"><a href="<?= BASE_URL; ?>" target="_blanck"><?= WEB_EMPRESA; ?></a></p>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</body>
+</html>
